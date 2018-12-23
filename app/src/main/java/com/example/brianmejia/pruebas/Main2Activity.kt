670b3ca10961ff72d58133260cd4e02b51617717
brianMejia05 <brianariel04@yahoo.es>
2018-12-23 13:42:18 -0600

    Agregando el 2do layout

--- app/src/main/java/com/example/brianmejia/pruebas/Main2Activity.kt
+++ app/src/main/java/com/example/brianmejia/pruebas/Main2Activity.kt
@@ -1,21 +1,12 @@
 package com.example.brianmejia.pruebas
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //inicio de las operaciones
-
-
-
-
+        setContentView(R.layout.activity_main2)
     }
 }
