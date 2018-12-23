5862ad575a179bfadb19e0360509a4b09499d95e
brianMejia05 <brianariel04@yahoo.es>
2018-12-23 13:13:19 -0600

    Initial commit

---
+++ app/src/main/java/com/example/brianmejia/pruebas/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.brianmejia.pruebas
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
